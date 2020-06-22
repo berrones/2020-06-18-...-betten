0ea6cbd7d548cf905983c7672c36cfbff3c26924
berrones <adam.berrones@gmail.com>
2020-06-22 16:03:31 -0400

    fromAuto

---
+++ R/R.R
@@ -0,0 +1,6 @@
+library(taskscheduleR)
+taskscheduler_create(taskname = 'monAfter',
+                     rscript = "C:/OIG/Trash OIG/2020-06-18 ... betten/R/R2.R",
+                     schedule = 'DAILY',
+                     starttime = '16:20',
+                     startdate = '06/22/2020')
---
+++ R/R2.R
@@ -0,0 +1,6 @@
+# R2
+system('git pull origin master')
+system('git add .')
+system('git status')
+system('git commit -m "fromAuto"')
+system('git push -u origin master')
